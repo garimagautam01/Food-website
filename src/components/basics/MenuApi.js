f8338c689c23393bb0b367eb32499f0d1b460d9a
garimagautam01 <garima2131096@akgec.ac.in>
2023-05-10 00:23:24 +0530

    first commit

---
+++ src/components/basics/MenuApi.js
@@ -0,0 +1,79 @@
+const Menu = [
+    {
+        id: 1,
+        image:  "image/maggi.jpg",
+        name: "Maggi",
+        category: "Breakfast",
+        price: "12rs",
+        description: " i love maggi, Maggi is my ever green favourite noodle"
+    },
+    {
+        id: 2,
+        image: "image/allu-pakora.webp" ,
+        name: "allu Pakoda",
+        category: "evening",
+        price: "20rs",
+        description: " aalu pakora is the best breakfast ever,it contains  more tasty"
+    },
+    {
+        id: 3,
+        image: "image/corn.jpg",
+        name: "corn",
+        category: "Breakfast",
+        price: "30rs",
+        description: " corn is the best breakfast,it contains too much sweet  more tasty"
+    },
+    {
+        id: 4,
+        image:  "image/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "50rs",
+        description: " chola is the best breakfast ever,it contains sppicy masala"
+    },
+    {
+        id: 5,
+        image:  "image/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "30rs",
+        description: " corn is the best breakfast ever,it contains too much sweet"
+    },
+    {
+        id: 6,
+        image:  "image/veg-thali.webp",
+        name: "veg-thali",
+        category: "dinner",
+        price: "180rs",
+        description: " veg thali is the best breakfast ever,it contains too much sweet which make it more tasty"
+    },
+    {
+        id: 7,
+        image:  "image/sweets.jpg",
+        name: "sweets",
+        category: "dinner",
+        price: "60rs",
+        description: "sweets are at the very low price in this reastaurant  the taste of sweets"
+    },
+    {
+        id: 8,
+        image: "image/rajma-chawal.jpg" ,
+        name: "rajma-rice",
+        category: "lunch",
+        price: "80rs",
+        description: " rajma-rice is the best breakfast ever,it contains too much rajma"
+    },
+    {
+        id: 9,
+        image:  "image/samosa.jpg" ,
+        name: "samosa",
+        category: "evening",
+        price: "10rs",
+        description: " samosa is the best breakfast ever,it contains too much potatoes"
+    },
+
+    
+];
+
+
+export default Menu;
